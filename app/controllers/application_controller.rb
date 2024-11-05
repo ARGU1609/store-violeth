b7896f051b190d940152133e19b6ad0683dad295
ARGU1609 <nelsonargumedo@gmail.com>
2024-11-04 21:04:06 -0500

    agregando devise admin

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,11 @@
 class ApplicationController < ActionController::Base
+  protected
+
+  def check_admin_priv
+    if !current_admin
+    redirect_to root_path 
+    end 
+  end 
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
 end
