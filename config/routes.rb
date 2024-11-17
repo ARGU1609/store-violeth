3c024aeceef3e5649ebcec1fb28e879c80db59f7
ARGU1609 <nelsonargumedo@gmail.com>
2024-11-17 11:00:31 -0500

    add user devise registration

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  resources :categories
   devise_for :admins
+  devise_for :users
+  resources :categories
   resources :products
   resource :admin, only: [:show], controller: :admin
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
